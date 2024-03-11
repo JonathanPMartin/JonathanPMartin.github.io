2017c6a7565df3af3f4fa556cec71e07b905a381
Jonathan <jonathan_P_Martin@outlook.com>
2024-03-11 22:29:28 +0000

    testing a fix for the redirect on the form

---
+++ action_page.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    include "index.html";
+    ?>
+    <p>test data exists here now</p>
+    <?php include "footer.html"; ?>
+  </body>
+</html>
